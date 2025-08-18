1f1e5393e21416d59face83111d7fe43ea44acc8
unknown <syjin@in4ucloud.com>
2025-08-18 18:52:31 +0900

    Feature: input 창 드래그에 따라 다른창 뜨도록 설정

--- src/components/buttons/HandMovingButton.tsx
+++ src/components/buttons/HandMovingButton.tsx
@@ -1,8 +1,26 @@
 import "./buttons.scss";
 
-const HandMovingButton = () => {
+type Props = {
+  onPointerDown: React.PointerEventHandler<HTMLDivElement>;
+  onPointerMove: React.PointerEventHandler<HTMLDivElement>;
+  onPointerUp: React.PointerEventHandler<HTMLDivElement>;
+  onPointerCancel: React.PointerEventHandler<HTMLDivElement>;
+};
+
+const HandMovingButton: React.FC<Props> = ({
+  onPointerDown,
+  onPointerMove,
+  onPointerUp,
+  onPointerCancel,
+}) => {
   return (
-    <div className="hand_moving_button">
+    <div
+      className="hand_moving_button"
+      onPointerDown={onPointerDown}
+      onPointerMove={onPointerMove}
+      onPointerUp={onPointerUp}
+      onPointerCancel={onPointerCancel}
+    >
       <div className="hand_moving_icon"></div>
     </div>
   );
--- src/containers/flightSearch/FlightSearch.tsx
+++ src/containers/flightSearch/FlightSearch.tsx
@@ -1,9 +1,14 @@
 import "./FlightSearch.scss";
 import moment from "moment";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import HandMovingButton from "@/components/buttons/HandMovingButton";
-// import FlightSearchSmall from "./FlightSearchSmall";
+import FlightSearchSmall from "./FlightSearchSmall";
 import FlightSearchBig from "./FlightSearchBig";
+import classNames from "classnames";
+
+const COLLAPSED_H = 140; // 닫힌 높이(px) - Small 보이는 상태
+const EXPANDED_H = 510; // 열린 높이(px) - Big 보이는 상태
+const SNAP_RATIO = 0.5; // 0~1 사이. 이 비율 이상이면 Big로 스냅
 
 const FlightSearch = () => {
   const [inputs, setInputs] = useState({
@@ -19,6 +24,12 @@ const FlightSearch = () => {
       secondStep: 2,
     },
   });
+  // 패널 상태
+  const [panelH, setPanelH] = useState(COLLAPSED_H);
+  const [expanded, setExpanded] = useState(false); // false=Small, true=Big
+  const [dragging, setDragging] = useState(false);
+  const startY = useRef(0);
+  const startH = useRef(panelH);
 
   useEffect(() => {
     setInputs(prev => ({
@@ -30,11 +41,63 @@ const FlightSearch = () => {
     }));
   }, [inputs.stayDuration.firstStep]);
 
+  // 드래그 시작
+  const onPointerDown = (e: React.PointerEvent<HTMLDivElement>) => {
+    e.currentTarget.setPointerCapture?.(e.pointerId);
+    setDragging(true);
+    startY.current = e.clientY;
+    startH.current = panelH;
+    document.body.style.userSelect = "none";
+    document.body.style.cursor = "grabbing";
+  };
+
+  // 드래그 중
+  const onPointerMove = (e: React.PointerEvent<HTMLDivElement>) => {
+    if (!dragging) return;
+    const dy = e.clientY - startY.current; // 아래로 당기면 + (높이 증가)
+    let nh = startH.current + dy;
+    if (nh < COLLAPSED_H) nh = COLLAPSED_H;
+    if (nh > EXPANDED_H) nh = EXPANDED_H;
+    setPanelH(nh);
+
+    // 드래그 중에도 실시간으로 컴포넌트 전환 느낌을 주고 싶으면 아래 라인 유지
+    setExpanded(nh >= COLLAPSED_H + (EXPANDED_H - COLLAPSED_H) * SNAP_RATIO);
+  };
+
+  // 드래그 끝
+  const onPointerUp = () => {
+    setDragging(false);
+    document.body.style.userSelect = "";
+    document.body.style.cursor = "";
+
+    const shouldExpand = panelH >= COLLAPSED_H + (EXPANDED_H - COLLAPSED_H) * SNAP_RATIO;
+    setExpanded(shouldExpand);
+    setPanelH(shouldExpand ? EXPANDED_H : COLLAPSED_H); // 스냅 애니메이션
+  };
+
   return (
-    <div className="flight_inputs">
-      {/* <FlightSearchSmall inputs={inputs} setInputs={setInputs} /> */}
-      <FlightSearchBig inputs={inputs} setInputs={setInputs} />
-      <HandMovingButton />
+    <div
+      className={classNames("flight_inputs", {
+        "is-expanded": expanded,
+        "is-collapsed": !expanded,
+        "is-dragging": dragging,
+      })}
+      style={{
+        height: panelH,
+        transition: dragging ? "none" : "height .25s ease",
+      }}
+    >
+      {expanded ? (
+        <FlightSearchBig inputs={inputs} setInputs={setInputs} />
+      ) : (
+        <FlightSearchSmall inputs={inputs} setInputs={setInputs} />
+      )}
+      <HandMovingButton
+        onPointerDown={onPointerDown}
+        onPointerMove={onPointerMove}
+        onPointerUp={onPointerUp}
+        onPointerCancel={onPointerUp}
+      />
     </div>
   );
 };
